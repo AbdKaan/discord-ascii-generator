14c98916ab0e0bae37a472b1107c7941fb5aca82
AbdKaan <abdu.kaan@gmail.com>
2024-10-24 22:23:03 +0300

    size will be under 2000 characters

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from image_converter import image_to_ascii
 
 
 def main():
-    image_path = "pog.png"
-    #27
-    #image_to_ascii(image_path=image_path)
+    image_path = "files/Aware.png"
+    image_to_ascii(image_path=image_path)
 
 main()
